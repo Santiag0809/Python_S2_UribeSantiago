d9f5639d679a1307a0a89789ecd54c7d601194e7
santiago uribe <santiuribeduarte@gmail.com>
2025-05-07 13:33:23 -0500

    :Feat: :truck: Pseint to python

--- Dia_2/Ejercicio_3.py
+++ Dia_2/Ejercicio_3.py
@@ -1,11 +1,6 @@
 # # Algoritmo para calcular el factorial de un número
-# # Se pide un número al usuario
 n = int(input("Ingresa un número: "))
-# # # Se inicializa la variable factorial en 1
 factorial = 1
-# # Se calcula el factorial utilizando un bucle for
 for i in range(1, n + 1):
-    # # Se multiplica el número actual por el factorial acumulado
     factorial *= i
-# # # Se imprime el resultado
 print("El factorial de:", n, "es:", factorial)
