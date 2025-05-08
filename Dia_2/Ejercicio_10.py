00957694e47531cad8ee69ab3618c72bc74e108f
Santiago Uribe <santiuribeduarte@gmail.com>
2025-05-07 23:56:48 -0500

    PythonS2 :smiley_cat:

--- Dia_2/Ejercicio_10.py
+++
@@ -1,9 +0,0 @@
-# # Algoritmo que pida un numero para hacer una lista y luego saque el promedio de los valores ingresados
-n = int(input("Hola, bienvenido porfavor inserte la cantidad "))
-totalnumeros = 0
-
-for i in range (n):
-    numeros = int(input("Ingrese las califaciones o numeros para su tabla "))
-    totalnumeros= totalnumeros + numeros
-promedio = totalnumeros / n
-print ("El promedio de tus notas o numeros es ", promedio)
--- Dia_2/Ejercicio_7.py
+++
@@ -1,5 +0,0 @@
-# # Algoritmo que calcule el area de un triangulo 
-base = int(input("Holaa, digite la base de su triangulo"))
-altura = int(input("Holaa, digite la altura de su triangulo"))
-area = (base*altura)/2
-print ("El area de su triangulo es ", area)
