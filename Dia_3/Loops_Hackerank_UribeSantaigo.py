3b88aa4b6ba2c4407d96c8c1a0c61be55b009521
Santiago <santiuribeduarte@gmail.com>
2025-05-09 11:37:34 -0500

    :feat::smiley_cat: Fixing mistakes and adding new files

--- Dia_3/Loops_Hackerank_UribeSantaigo.py
+++ Dia_3/Loops_Hackerank_UribeSantaigo.py
@@ -1,4 +1,6 @@
 # Algoritmo que imprima el cuadrado de un numero desde 0 hasta n 
 num = int(input("Bienvenido,ingrese un numero "))
+imprimir = ""
 for i in range(num):
-    print(i**2, end=" ")
+    imprimir += str( i**2) + " "
+print (imprimir)
---
+++ Dia_3/PythonDivision_Hackerank_UribeSantiago.py
@@ -0,0 +1,5 @@
+# # Algoritmo que divide dos numeros y da su resultado en entero y flotante
+a = int(input("Ingrese el primer numero"))
+b = int(input("Inrese el segundo numero"))
+print("EL resultado entero es ",a//b)
+print("El resultado entero es ",a/b)
